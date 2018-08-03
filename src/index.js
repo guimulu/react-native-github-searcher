c07c51317bd922657e8a280162c0101e9515c83b
Guilherme Muller Sagrilo Rodrigues <guilherme.rodrigues@meta.com.br>
2018-08-03 14:49:14 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './config/ReactoTronConfig';
+
+import { AsyncStorage, YellowBox } from 'react-native';
+
+import createNavigator from 'routes';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  }
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@appGithub:username');
+
+    this.appLoaded(username);
+  }
+
+  appLoaded = (username) => {
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    if (!this.state.userChecked) return null;
+    const Routes = createNavigator(this.state.userLogged);
+    return <Routes />;
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import HeaderRight from 'components/HeaderRight';
+import Welcome from 'pages/welcome';
+import Repositories from 'pages/repositories';
+import Organizations from 'pages/organizations';
+import { metrics, colors } from 'styles';
+
+const createNavigator = (isLogged = false) => StackNavigator({
+  Welcome: { screen: Welcome },
+  User: {
+    screen: TabNavigator({
+      Repositories: { screen: Repositories },
+      Organizations: { screen: Organizations },
+    }, {
+      tabBarPosition: 'bottom',
+      tabBarOptions: {
+        showIcon: true,
+        showLabel: false,
+        activeTintColor: colors.white,
+        inactiveTintColor: colors.whiteTransparent,
+        style: {
+          backgroundColor: colors.secundary,
+        },
+      },
+    }),
+  },
+}, {
+  initialRouteName: isLogged ? 'User' : 'Welcome',
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      paddingHorizontal: metrics.basePadding,
+    },
+    headerRight: <HeaderRight navigation={navigation} />,
+  }),
+});
+
+export default createNavigator;
